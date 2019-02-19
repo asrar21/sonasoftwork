ced577a606c6e9812c203bffca3c55914c5916d5
faizan <faizankhurshid23@gmail.com>
2019-02-19 16:43:11 +0500

    sf

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import HomeScreen from "./Screens/HomeScreen/homeScreen";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <HomeScreen />        
+      </div>
+    )
+  }
+}
---
+++ src/Screens/HomeScreen/homeScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Navbar from "../../Containers/Navbar/navbar"
+
+export default class homeScreen extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    )
+  }
+}
