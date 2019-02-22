65fb303b9f17f3d107b4a210e9bbc3b1e8b08f75
faizi23 <faizankhurshid23@gmail.com>
2019-02-22 16:39:20 +0500

    changed generic file name to specific

--- src/Screens/HomeScreen/homeScreen.js
+++ src/Screens/HomeScreen/homeScreen.js
@@ -4,7 +4,7 @@ import { Grommet, Box} from "grommet";
 //impoter theme from grommet
 import { grommet } from "grommet/themes";
 import Navbar from "../../Containers/Navbar/navbar";
-import Card from "../../Containers/Card/card"
+import HomeScreenCard from "../../Containers/homescreenCard/homeScreenCard"
 
  class HomeScreen extends Component {
   render() {
@@ -18,15 +18,16 @@ import Card from "../../Containers/Card/card"
           
           {/*container for 8 cards */}
           <Box direction="row" justify="center" wrap={true} >
-              {/* Card is a component made using grommet box*/}
-              <Card iconName="information_blue" heading="INFORMATION GOVERNANCE" link1="Dashboard" link2="Content identification" link3="Data Dispostion" />
-              <Card iconName="identification_blue" heading="IDENTIFICATION" link1="Search Archive" link2="My Archived Email" />
-              <Card iconName="preservation_blue" heading="PRESERVATION" link1="Case" link2="Legal Hold" />
-              <Card iconName="processing_blue" heading="PROCESSING" />
-              <Card iconName="review_blue" heading="REVIEW" />
-              <Card iconName="analysis_blue" heading="ANALYSIS" />
-              <Card iconName="production_blue" heading="PRODUCTION" link1="Export" />
-              <Card iconName="reporting_blue" heading="REPORTING" />
+
+              {/* HomeScreenCard is a component made using grommet box*/}
+              <HomeScreenCard iconName="information_blue" heading="INFORMATION GOVERNANCE" link1="Dashboard" link2="Content identification" link3="Data Dispostion" />
+              <HomeScreenCard iconName="identification_blue" heading="IDENTIFICATION" link1="Search Archive" link2="My Archived Email" />
+              <HomeScreenCard iconName="preservation_blue" heading="PRESERVATION" link1="Case" link2="Legal Hold" />
+              <HomeScreenCard iconName="processing_blue" heading="PROCESSING" />
+              <HomeScreenCard iconName="review_blue" heading="REVIEW" />
+              <HomeScreenCard iconName="analysis_blue" heading="ANALYSIS" />
+              <HomeScreenCard iconName="production_blue" heading="PRODUCTION" link1="Export" />
+              <HomeScreenCard iconName="reporting_blue" heading="REPORTING" />
 
           </Box>
           
