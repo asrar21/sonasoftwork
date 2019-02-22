3c5c4082ab1655957f5ec64a286d762d6f14d0eb
faizi23 <faizankhurshid23@gmail.com>
2019-02-22 14:04:24 +0500

    done with homescreen responsiveness

--- src/Screens/HomeScreen/homeScreen.js
+++ src/Screens/HomeScreen/homeScreen.js
@@ -1,328 +1,35 @@
 import React, { Component } from "react";
-//importing iconimages from icons floder
-import * as Icons from "./homeScreenIcons"
 //imported some components from Gromet
-import { Grommet, Box, Grid, Text ,Image,Paragraph} from "grommet";
+import { Grommet, Box} from "grommet";
 //impoter theme from grommet
 import { grommet } from "grommet/themes";
 import Navbar from "../../Containers/Navbar/navbar";
+import Card from "../../Containers/Card/card"
 
- export default class Grommets extends Component {
-
-
+ class HomeScreen extends Component {
   render() {
-   
     return (
        // wrapping all the box component in a grommet component 
       <Grommet full theme={grommet}>
-      {/* using grid for positioning the header and main content */}
-        {/* <Grid
-          fill
-          rows={["auto", "flex"]}
-          columns={["auto", "flex"]}
-          areas={[
-           { name: "header", start: [0, 0], end: [1, 0] },
-          
-            { name: "main", start: [1, 1], end: [1, 1] },
-            // { name: "footer", start: [0,0], end: [1,1 ] },
-            
-          ]}
-        > */}
-        {/* the  header box */} 
-          <Box
-          gridArea="header"
-          pad="none"
-          >
+          {/* conatiner for navbar */}
+          <Box pad="none" >
             <Navbar />
           </Box>
-          {/* <Box
-            gridArea="header"
-            direction="row"
-            align="center"
-            justify="between"
-            pad={{ horizontal: "medium", vertical: "small" }}
-            background="#163552"
-            
-            border={{side:"bottom",color:"orange" ,size:"medium"}}
-          >
-            
-              <Text color="lightgrey"  size="xxlarge" weight="bold">SonaVault</Text>
-              <Image src={Icons.profile_gray} width="50px" height="50px"/>
           
-          </Box> */}
-          
-        {/* the wrapper box which contains all the 8 boxes */}
-      <Box   
-          
-      direction="row"
-      justify="start"
-      align="center"
-      // pad={{left:"medium"}}
-      // pad="medium"
-      margin="none"
-      pad="none"
-      border={{side:"all", color:"orange", size:"small"}}
-      background="transparent"
-      wrap={true}
-      fill
-      gap="100px"
-      >
-        {/* box for information governance */}
-      <Box
-        pad="medium"
-        direction="column"
-        align="center"
-        alignContent="center"
-        background={{ color: "light-2", opacity: "strong" }}
-        width={"235px"}
-        margin="none"
-        // margin={{left:"40px"}}
-        height={"235px"}
-        gap="none"
-      >
-      <Image src={Icons.information_blue} fit="contain" />
-       
-       <Text size="small" color=" #362969" border={{side:"bottom", color: "orange"}}> INFORMATION GOVERNANCE</Text>
-       <Paragraph size="small" margin="none" color=" #362969"
-               >
-        DashBoard
-       
-        
-         </Paragraph>
+          {/*container for 8 cards */}
+          <Box direction="row" justify="center" wrap={true} >
+              {/* Card is a component made using grommet box*/}
+              <Card iconName="information_blue" heading="INFORMATION GOVERNANCE" link1="Dashboard" link2="Content identification" link3="Data Dispostion" />
+              <Card iconName="identification_blue" heading="IDENTIFICATION" link1="Search Archive" link2="My Archived Email" />
+              <Card iconName="preservation_blue" heading="PRESERVATION" link1="Case" link2="Legal Hold" />
+              <Card iconName="processing_blue" heading="PROCESSING" />
+              <Card iconName="review_blue" heading="REVIEW" />
+              <Card iconName="analysis_blue" heading="ANALYSIS" />
+              <Card iconName="production_blue" heading="PRODUCTION" link1="Export" />
+              <Card iconName="reporting_blue" heading="REPORTING" />
 
-       <Paragraph size="small" margin="none" color=" #362969">
-       Content Identification
-
-       </Paragraph>
-       <Paragraph size="small" margin="none" color=" #362969">
-       Data Disposition
-
-       </Paragraph>
-       
-       
-      </Box>
-       {/*box for identification */}
-      <Box
-        pad="small"
-        direction="column"
-        align="center"
-        alignContent="center"
-        background={{ color: "light-2", opacity: "strong" }}
-        width={"235px"}
-        // margin={{left:"33px"}}
-        margin="none"
-        height={"235px"}
-        
-        gap="none"
-      >
-      <Image src={Icons.identification_blue} fit="contain" />
-       
-       <Text size="small"  color=" #362969"> IDENTIFICATION</Text>
-       <Paragraph margin="none"
-               color=" #362969">
-        Search Archived
-       
-         </Paragraph>
-        <Paragraph margin="none"
-               color=" #362969">
-        My  Archived Email
-       
-        </Paragraph>
-
-       
-       
-      </Box>
-      {/* box for preservation */}
-
-      {<Box
-        pad="medium"
-        direction="column"
-        align="center"
-        alignContent="center"
-        background={{ color: "light-2", opacity: "strong" }}
-        width={"235px"}
-        // margin={{left:"33px"}}
-        margin="none"
-        height={"235px"}
-        gap="none"
-      >
-      <Image src={Icons.preservation_blue} fit="contain" />
-       
-       <Text size="small" color=" #362969"> PRESERVATION</Text>
-       <Paragraph margin="none"
-              color=" #362969" >
-        Case
-       
-        
-       </Paragraph>
-
-       <Paragraph margin="none" color=" #362969">
-       Legal Hold
-
-       </Paragraph>
-      
-       
-      </Box>
-      /* box for processing */}
-      <Box
-        pad="medium"
-        direction="column"
-        align="center"
-        alignContent="center"
-        background={{ color: "light-2", opacity: "strong" }}
-        width={"235px"}
-        // margin={{left:"33px"}}
-        margin="none"
-        height={"235px"}
-        gap="none"
-      >
-      <Image src={Icons.processing_blue} fit="contain" />
-       
-       <Text size="small" color=" #362969"> PROCESSING</Text>
-       <Paragraph margin="none"
-               >
-     
-       
-        
-       </Paragraph>
-
-       <Paragraph margin="none">
-      
-
-       </Paragraph>
-      
-       
-      </Box>
-     
-   {/* box for review */}
-      <Box
-        pad="medium"
-        direction="column"
-        align="center"
-        alignContent="center"
-        background={{ color: "light-2", opacity: "strong" }}
-        width={"235px"}
-        // margin={{left:"33px"}}
-        margin="none"
-        height={"235px"}
-        gap="none"
-      >
-      <Image src={Icons.review_blue} fit="contain" />
-       
-       <Text size="small" color=" #362969"> REVIEW</Text>
-       <Paragraph margin="none"
-               >
-     
-       
-        
-       </Paragraph>
-
-       <Paragraph margin="none">
-      
-
-       </Paragraph>
-      
-       
-      </Box>
-      {/* box for analysis */}
-      <Box
-        pad="medium"
-        direction="column"
-        align="center"
-        alignContent="center"
-        background={{ color: "light-2", opacity: "strong" }}
-        width={"235px"}
-        // margin={{left:"33px"}}
-        margin="none"
-        height={"235px"}
-        gap="none"
-      >
-      
-      <Image src={Icons.analysis_blue} fit="contain" />
-       
-       <Text size="small" color=" #362969"> ANALYSIS</Text>
-       <Paragraph margin="none"
-               >
-     
-       
-        
-       </Paragraph>
-
-       <Paragraph margin="none">
-      
-
-       </Paragraph>
-      
-       
-      </Box>
-      {/* box for production  */}
-      <Box
-        pad="medium"
-        direction="column"
-        align="center"
-        alignContent="center"
-        background={{ color: "light-2", opacity: "strong" }}
-        width={"235px"}
-        // margin={{left:"33px"}}
-        margin="none"
-        height={"235px"}
-        gap="none"
-      >
-      <Image src={Icons.production_blue} fit="contain" />
-       
-       <Text size="small" color=" #362969"> PRODUCTION</Text>
-       <Paragraph margin="none"
-               color=" #362969">
-               Export
-     
-       
-        
-       </Paragraph>
-
-       <Paragraph margin="none">
-      
-
-       </Paragraph>
-      
-       
-      </Box>
-      {/* box for reporting */}
-      <Box
-        pad="medium"
-        direction="column"
-        align="center"
-        alignContent="center"
-        background={{ color: "light-2", opacity: "strong" }}
-        width={"235px"}
-        // margin={{left:"33px"}}
-        margin="none"
-        height={"235px"}
-        gap="none"
-      >
-      <Image src={Icons.reporting_blue} fit="contain" />
-       
-       <Text size="small" color=" #362969"> REPORTING</Text>
-       <Paragraph margin="none"
-              color=" #362969" >
-     
-       
-        
-       </Paragraph>
-
-       <Paragraph margin="none">
-      
-
-       </Paragraph>
-      
-       
-    
-        </Box> 
           </Box>
-         
-        
-        {/* </Grid> */}
-       
-       
+          
       </Grommet>
      
     );
@@ -330,3 +37,4 @@ import Navbar from "../../Containers/Navbar/navbar";
 }
 
 
+export default HomeScreen;
--- src/Screens/Login/login.js
+++ src/Screens/Login/login.js
@@ -1,155 +1,138 @@
-import React, { Component } from 'react';
-//importing components from grommet
-import { Grommet, Box, ResponsiveContext,Button,Image, Text , Heading, TextInput, Markdown} from "grommet";
-//imported icon from assets/Icons Folder
-import sonaLogo from "../../assets/Icons/sonaLogo.png"
-//imported icon from grommeticons library
-import { Login,CircleInformation } from 'grommet-icons';
+  import React, { Component } from 'react';
+  //importing components from grommet
+  import { Grommet, Box, ResponsiveContext,Button,Image, Text , Heading, TextInput, Markdown} from "grommet";
+  //imported icon from assets/Icons Folder
+  import sonaLogo from "../../assets/Icons/sonaLogo.png"
+  //imported icon from grommeticons library
+  import { Login, CircleInformation } from 'grommet-icons';
 
 
-class LogIn extends Component {
-  constructor(props){
-    super(props)
-    this.state = {}
-  }
+  class LogIn extends Component {
+    constructor(props){
+      super(props)
+      this.state = {}
+    }
 
-  
-  render() {
-    const myTheme = {
-      global:{
-        font: {
-          family: 'Roboto',
-        },
-        control:{
-          border:{
-            width: "2px",
+    
+    render() {
+      const myTheme = {
+        global:{
+          font: {
+            family: 'Roboto',
+          },
+          control:{
+            border:{
+              width: "2px",
+            },
           },
         },
-      },
-    }
-    return (
-      <div>
-        {/* using Grommet to wrap all the grommet component */}
-        <Grommet full theme={myTheme}>
+      }
+      return (
+        <div>
+          {/* using Grommet to wrap all the grommet component */}
+          <Grommet full theme={myTheme}>
+          
         
-       
-        {/* main Box which holds all the boxes */}
-          <Box
-            fill
-            pad="none"
-            margin="none"
-            direction="row"
-            align="stretch"
-            alignContent="stretch"
-          >
-          {/* to make the box responsive we used this */}
-            <ResponsiveContext.Consumer>
-              {(size) => (
-                size === "medium" ?  (
-            // sidebar box
-                <Box 
-                  fill 
-                  margin="none" 
-                  pad="medium" 
-                  background="#2e3c54"  
-                  direction="column"
-                  justify="center"
-                  height="xxlarge"
-                  align="center"
-                  alignContent="start"
-                  alignSelf="stretch"
-                  basis="1/4" >
-                  <Box height="small" pad="small" width="small"  >
-                  <Image src={sonaLogo} fit="contain" margin="small" alignSelf="center"/>
-                  <Text textAlign="center" size="small">Version 6.5.0.0</Text>
-                  </Box>
-
-                </Box>  
-                
-                
-                )
-                : null 
-                
-              )}
-            </ResponsiveContext.Consumer>
-          
-            
-             
-              
-            
-            {/* box which contain textboxes ,labeling and icon button */}
-            <Box 
-            
-              justify="start"
-              align="start"
-              direction="column"
-              background="white" 
-              pad="small" 
-              margin="none" 
-              basis="1" 
-          
-              width={"100%"} 
-            
-            >
-           
-                
-           <Box  
-              justify="end"
-              align="start"
-              alignContent="start"
-             width={"100%"}
-              // basis="1"
-              direction="row"
-              margin="none"
+          {/* main Box which holds all the boxes */}
+            <Box
+              fill
               pad="none"
-              
+              margin="none"
+              direction="row"
+              align="stretch"
+              alignContent="stretch"
             >
-              <CircleInformation margin="none" pad="none" textDecoration="none" />
-              </Box>
-             
-              <Box 
-                basis="1" 
-                height="medium" 
-                width="300px"
-                justify="end"
-                margin="medium"
-              // align="end"
-              alignSelf="center" 
-               
-                
-              >
-                <Heading  
-                  size="small" 
-                  alignSelf ="start" 
-                >
-                <Markdown>**SonaVault Login**</Markdown>
-                </Heading>
-                <Text alignSelf="start"  size="medium">Email :</Text>
-                <TextInput size="small" placeholder="Enter your email" name="Email" onChange={(event) => {/* event.target.value */}} />
-                <Text  alignSelf="start"  size="medium">Password :</Text>
-                <TextInput size="small" placeholder="Enter your password" name="Password" onChange={(event) => {/* event.target.value */}} />
-                <Box pad ="20px">
-                  <Button 
-                    icon={<Login />}
-                    label="sign in"
-                    alignSelf="start"
-                    plain="True"
-                    onClick={() => this.props.navigateToHomeScreen()}
-                  />
-                </Box>
-              </Box>
-              
-            </Box>
-            
-           
-           </Box>
-            
-          
-         
-        </Grommet>
-      </div>
-    )
-  }
-}
+            {/* to make the box responsive we used this */}
+              <ResponsiveContext.Consumer>
+                {(size) => (
+                  size === "medium" ?  (
+              // sidebar box
+                  <Box 
+                    fill 
+                    pad="medium" 
+                    background="#2e3c54"  
+                    direction="column"
+                    justify="center"
+                    align="center"
+                    basis="1/4" 
+                  >
+                      <Box height="small" pad="small" width="small"  >
+                        <Image src={sonaLogo} fit="contain" margin="small" alignSelf="center"/>
+                          <Text textAlign="center" size="small">Version 6.5.0.0</Text>
+                      </Box>
 
-export default LogIn;
+                  </Box>  
+                  
+                  
+                  )
+                  : null 
+                  
+                )}
+              </ResponsiveContext.Consumer>
+            
+              
+              
+                
+              
+              {/* box which contain textboxes ,labeling and icon button */}
+              <Box 
+                justify="start"
+                align="start"
+                direction="column"
+                background="white" 
+                pad="small" 
+                margin="none" 
+                basis="1" 
+            
+                width={"100%"} 
+              
+              >
+            
+                  
+                <Box  
+                    justify="end"
+                    align="start"
+                    width={"100%"}
+                    direction="row"
+                  >
+                    <CircleInformation cursor="pointer" textDecoration="none" />
+                </Box>
+              
+                <Box 
+                  height="medium" 
+                  width="300px"
+                  justify="end"
+                  alignSelf="center" 
+                >
+                    <Heading  
+                      size="small" 
+                      alignSelf ="start" 
+                    >
+                        <Markdown>**SonaVault Login**</Markdown>
+                    </Heading>
+                    <Text alignSelf="start"  size="medium">Email :</Text>
+                    <TextInput size="small" placeholder="Enter your email" name="Email" onChange={(event) => {/* event.target.value */}} />
+                    <Text  alignSelf="start"  size="medium">Password :</Text>
+                    <TextInput size="small" type="password" placeholder="Enter your password" name="Password" onChange={(event) => {/* event.target.value */}} />
+                    <Box pad ="20px">
+                        <Button 
+                          icon={<Login />}
+                          label="sign in"
+                          alignSelf="start"
+                          plain="True"
+                          onClick={() => this.props.navigateToHomeScreen()}
+                        />
+                    </Box>
+                </Box>
+                
+              </Box>
+          
+            </Box>
+              
+          </Grommet>
+        </div>
+      )
+    }
+  }
+
+  export default LogIn;
