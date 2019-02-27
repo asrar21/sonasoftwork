ba823889a5bfc64a35721b8db60d0470d3ae6184
asrara <asrara@sonasoft.com>
2019-02-27 11:34:11 +0500

    updated code

--- src/Screens/Configuration/Smtpconfiguration.js
+++ src/Screens/Configuration/Smtpconfiguration.js
@@ -6,6 +6,7 @@ import {
     TextInput,
     Button,
     CheckBox,
+    Heading
 
 } from "grommet";
 
@@ -15,102 +16,163 @@ import {
 export default class SMTPconfiguration extends Component {
 
 
+    constructor(props) {
+        super(props);
+        this.state = {
 
+            ischecked:false,
+            
+        };
+    }
 
 
 
 
     render() {
+        //theme of save button
+        const Theme = {
+            button: {
+                border: {
+                    radius: undefined,
+                    color: "#2196f3"
+                },
+                primary: {
+                    color: "#2196f3"
+                },
+                extend: {
+                    color: "white"
+                }
+            }
+        }
+        //theme of Cancel button
+        const Theme1 = {
+            button: {
+                border: {
+                    radius: undefined,
+                    color: "dark-3"
+                },
+                primary: {
+                    color: "white"
+                },
+            }
+        }
 
 
         return (
             <div>
-                <Box border={{ side: "all", color: "black" }}>
-                    <Box direction="row-responsive" justify="center">
+                <Box direction="row"
+                    border={{ color: "light-3" }}>
+                    <Box width="medium"
+                        border={{ color: "light-3" }}>
                         <Text>
-                            Sender Email:
+                            Senders Email:
                                     </Text>
-                        <Box width={"70%"} margin={{ left: "large" }}>
+                    </Box>
+                    <Box direction="row">
+                        <Box>
                             <TextInput value="bilala@sonasoft.com"></TextInput>
                         </Box>
+
                     </Box>
-                    <Box direction="row-responsive" margin={{ top: "large" }} justify="center" >
+                    </Box>
+
+                    <Box direction="row"
+                    border={{ color: "light-3" }}>
+                    <Box width="medium"
+                        border={{ color: "light-3" }}>
                         <Text>
                             SMTP Host:
                                     </Text>
-                        <Box direction="row-responsive" width={"70%"} margin={{ left: "large" }} >
+                    </Box>
+                    <Box direction="row">
+                        <Box>
                             <TextInput value="smtp.office365.com"></TextInput>
-
                         </Box>
 
                     </Box>
-                    <Box direction="row-responsive" margin={{ top: "large" }} justify="start" align="center" >
-                        <Box margin={{ left: "170px" }}>
-                            <Text>
-                                Port:
+                    </Box>
+                    <Box direction="row"
+                    border={{ color: "light-3" }}>
+                    <Box width="medium"
+                        border={{ color: "light-3" }}>
+                        <Text>
+                            Port:
                                     </Text>
-                        </Box>
-
-                        <Box direction="row-responsive" width={"5%"} margin={{ left: "xlarge" }} >
-                            <TextInput value=""></TextInput>
-
+                    </Box>
+                    <Box direction="row">
+                        <Box  width={"20%"}>
+                            <TextInput ></TextInput>
                         </Box>
 
                     </Box>
-                    <Box direction="row-responsive" margin={{ top: "large" }} justify="start" align="center" >
-                        <Box margin={{ left: "170px" }}>
-                            <Text>
-                                Authenthication Required:
+                    </Box>
+                    <Box direction="row"
+                    border={{ color: "light-3" }}>
+                    <Box width="medium"
+                        border={{ color: "light-3" }}>
+                        <Text>
+                            Authenthication Required:
                                     </Text>
-                        </Box>
-                        <Box direction="row-responsive" width={"25%"} margin={{ left: "large" }} >
-                            <CheckBox></CheckBox>
-
+                    </Box>
+                    <Box direction="row">
+                        <Box>
+                           <CheckBox
+                             checked={this.state.ischecked}
+                             onChange={(event) => this.setState({ischecked:event.target.checked})}/>
                         </Box>
 
                     </Box>
-
-                    <Box>
-                        <Box margin={{ top: "large" }} border={{ side: "all", color: "black" }}>
-                            <Box pad="medium" margin="medium" border={{ side: "all", color: "black" }}>
-                                <Text>Cerendials</Text>
+                    </Box>
+                     <Box >
+                                <Heading size="small">Cerendials</Heading>
                             </Box>
 
-                            <Box direction="row-responsive" justify="center">
-                                <Text>
-                                    User Name:
+                            <Box direction="row"
+                    border={{ color: "light-3" }}>
+                    <Box width="medium"
+                        border={{ color: "light-3" }}>
+                        <Text>
+                            UserName:
                                     </Text>
-                                <Box width={"70%"} margin={{ left: "large" }}>
-                                    <TextInput value="bilala@sonasoft.com"></TextInput>
-                                </Box>
-                            </Box>
-                            <Box direction="row-responsive" margin={{ top: "large" }} justify="center" pad="small">
-
-                                <Text>Password:  </Text>
-                                <Box width={"70%"} margin={{ left: "large" }} justify="center">
-                                    <TextInput value="*******"></TextInput>
-                                </Box>
-                                
-
-
-                            </Box>
-                            <Box align="center" justify="center" direction="row-responsive" gap="medium" pad="">
-                                <Button label="save" /> <Button label="cancel" />
-                            </Box>
-                            
-
-
-
-
-
+                    </Box>
+                    <Box direction="row">
+                        <Box>
+                           <TextInput value="bilala@sonasoft.com"></TextInput>
                         </Box>
-                        
 
                     </Box>
+                    </Box>
+                    <Box direction="row"
+                    border={{ color: "light-3" }}>
+                    <Box width="medium"
+                        border={{ color: "light-3" }}>
+                        <Text>
+                            Password:
+                                    </Text>
+                    </Box>
+                    <Box direction="row">
+                        <Box>
+                           <TextInput value="*******"></TextInput>
+                        </Box>
+
+                    </Box>
+                    </Box>
+                    <Box justify="center" align="center" direction="row-responsive">
+                        <Button label="Save">
+
+                        </Button>
+                        <Button label="Cancel">
+
+                        </Button>
+                    </Box>
+
+                    
+
+                   
 
 
 
-                </Box>
+           
 
               
             </div>
--- src/Screens/Configuration/configComponents/Deploymentsetting.js
+++ src/Screens/Configuration/configComponents/Deploymentsetting.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 //importing components from grommet
 import {
-RadioButton,
-Text,
-Box,
-TextInput,
-Button,
-CheckBox,
-DataTable
+    RadioButton,
+    Text,
+    Box,
+    TextInput,
+    Button,
+    CheckBox,
+    DataTable,
+    Heading
 } from "grommet";
 
 
@@ -39,7 +40,7 @@ export default class Deploymentsetting extends Component {
         super(props);
         this.state = {
 
-
+            ischecked:false,
             centralArchive: true,
             multiArchive: false
         };
@@ -62,24 +63,63 @@ export default class Deploymentsetting extends Component {
 
 
     render() {
-        const { centralArchive, multiArchive } = this.state
+        const { centralArchive, multiArchive } = this.state;
+        //theme of save button
+        const Theme = {
+            button: {
+                border: {
+                    radius: undefined,
+                    color: "#2196f3"
+                },
+                primary: {
+                    color: "#2196f3"
+                },
+                extend: {
+                    color: "white"
+                }
+            }
+        }
+        //theme of Cancel button
+        const Theme1 = {
+            button: {
+                border: {
+                    radius: undefined,
+                    color: "dark-3"
+                },
+                primary: {
+                    color: "white"
+                },
+            }
+        }
 
         return (
             <div>
-                <Box border={{side:"all",color:"black"}}>
-                    <Box direction="row-responsive" justify="center">
+                <Box direction="row"
+                    border={{ color: "light-3" }}>
+                    <Box width="medium"
+                        border={{ color: "light-3" }}>
                         <Text>
                             Site prefix:
                                     </Text>
-                        <Box width={"70%"} margin={{ left: "large" }}>
+                    </Box>
+                    <Box direction="row">
+                        <Box>
                             <TextInput></TextInput>
                         </Box>
+
                     </Box>
-                    <Box direction="row-responsive" margin={{ top: "large" }} justify="center" >
+                </Box>
+
+                <Box direction="row"
+                    border={{ color: "light-3" }}>
+                    <Box width="medium"
+                        border={{ color: "light-3" }}>
                         <Text>
-                            Archival topology:
+                            Archival Topology:
                                     </Text>
-                        <Box direction="row-responsive" width={"70%"} margin={{ left: "small" }} >
+                    </Box>
+                    <Box direction="row">
+                        <Box direction="row-responsive">
                             <RadioButton label="central Archive" value="centralArchive" checked={centralArchive} name="archiveOptions" onChange={(e) => this.Show(e)}>
 
                             </RadioButton>
@@ -89,59 +129,86 @@ export default class Deploymentsetting extends Component {
                         </Box>
 
                     </Box>
-                    {this.state.multiArchive ?
-                        <Box>
-                            <Box margin={{ top: "large" }} border={{ side: "all", color: "black" }}>
-                                <Box pad="medium" margin="medium" border={{ side: "all", color: "black" }}>
-                                    <Text>Add other sites</Text>
-                                </Box>
-
-                                <Box direction="row-responsive" justify="center">
-                                    <Text>
-                                        Site URL:
-                                    </Text>
-                                    <Box width={"70%"} margin={{ left: "large" }}>
-                                        <TextInput></TextInput>
-                                    </Box>
-                                </Box>
-                                <Box direction="row-responsive" margin={{ top: "large" }} justify="center">
-
-                                    <Text>AuthorizedKey:  </Text>
-                                    <Box width={"70%"} margin={{ left: "large" }} justify="center">
-                                        <TextInput></TextInput>
-                                    </Box>
-
-
-                                </Box>
-                                <Box direction="row-responsive" margin={{ top: "large" }} justify="center">
-
-                                    <Text>AuthenthicationRequired:  </Text>
-                                    <Box width={"70%"} margin={{ left: "large" }} direction="row-responsive">
-                                        <CheckBox></CheckBox>
-                                        <Button label="Add"></Button>
-                                    </Box>
-
-                                </Box>
-                                <DataTable
-                                    columns={column.map(c => ({
-                                        ...c,
-                                        search: c.property === "SiteName" || c.property === "SearchUrl" || c.property === "Remove"
-                                    }))}
-                                    data={DATA}
-                                    sortable
-                                    resizeable
-                                />
-
-
-
+                </Box>
+                {this.state.multiArchive ?
+                    <Box>
+                        <Box >
+                            <Box >
+                                <Heading size="small">Add other sites</Heading>
                             </Box>
+
+                            <Box direction="row"
+                                border={{ color: "light-3" }}>
+                                <Box width="medium"
+                                    border={{ color: "light-3" }}>
+                                    <Text>
+                                        Site Url:
+                                    </Text>
+                                </Box>
+                                <Box direction="row">
+                                    <Box>
+                                        <TextInput></TextInput>
+                                    </Box>
+
+                                </Box>
+                            </Box>
+                            <Box direction="row"
+                                border={{ color: "light-3" }}>
+                                <Box width="medium"
+                                    border={{ color: "light-3" }}>
+                                    <Text>
+                                        Authorization Key:
+                                    </Text>
+                                </Box>
+                                <Box direction="row">
+                                    <Box>
+                                        <TextInput></TextInput>
+                                    </Box>
+
+                                </Box>
+                            </Box>
+                            <Box direction="row"
+                                border={{ color: "light-3" }}>
+                                <Box width="medium"
+                                    border={{ color: "light-3" }}>
+                                    <Text>
+                                        Authenthication Required:
+                                    </Text>
+                                </Box>
+                                <Box direction="row">
+                                    <Box>
+                                        <CheckBox 
+                                          checked={this.state.ischecked}
+                                          onChange={(event) => this.setState({ischecked:event.target.checked})}/>
+                                    </Box>
+
+                                </Box>
+                            </Box>
+                            <Box direction="row"
+                                >
+                                <Box >
+                            <DataTable
+                                columns={column.map(c => ({
+                                    ...c,
+                                    search: c.property === "SiteName" || c.property === "SearchUrl" || c.property === "Remove"
+                                }))}
+                                data={DATA}
+                                sortable
+                                resizeable
+                            />
+                            </Box>
+                            </Box>
+
+
 
                         </Box>
 
-                        : null
-                    }
+                    </Box>
 
-                </Box>
+                    : null
+                }
+
+
 
                 <Box align="center" justify="center" direction="row-responsive" gap="medium" pad="medium">
                     <Button label="save" /> <Button label="cancel" />
@@ -153,3 +220,5 @@ export default class Deploymentsetting extends Component {
         )
     }
 }
+
+
